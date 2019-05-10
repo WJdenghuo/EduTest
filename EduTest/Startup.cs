5aeb54438fdb313e4214689e81d0fd8300c11740
帅 郭 <wanjia_denghuo@sina.com>
2019-05-10 08:45:52 +0800

    增加进程间通信MediatR

--- EduTest/Startup.cs
+++ EduTest/Startup.cs
@@ -118,7 +118,7 @@ namespace EduTest
                 var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
                 var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
                 c.IncludeXmlComments(xmlPath);
-            });           
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
