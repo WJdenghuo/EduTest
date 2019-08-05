39f42f2b27447ae9edbb0f95bde7939eb9167c62
帅 郭 <wanjia_denghuo@sina.com>
2019-08-05 10:51:39 +0800

    janus 及docker调整

--- EduTest/Controllers/JanusController.cs
+++ EduTest/Controllers/JanusController.cs
@@ -3,11 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace EduTest.Controllers
 {
     public class JanusController : Controller
     {
+        private readonly ILogger _logger;
+        public JanusController(ILogger<JanusController> logger)
+        {
+            _logger = logger;
+        }
         public IActionResult Index()
         {
             return View();
@@ -18,7 +24,22 @@ namespace EduTest.Controllers
         }
         public IActionResult RoomTest()
         {
+            //异常筛选器测试
+            try
+            {
+
+            }
+            catch (Exception e) when (LogException(e))
+            {
+
+                throw;
+            }
             return View();
+        }
+        public Boolean LogException(Exception e)
+        {
+            _logger.LogError(e,e.Message,nameof(JanusController));
+            return false;
         }
     }
 }
