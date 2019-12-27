f93a6a5421d5354315dd1410ca7c2fb4e41f8731
帅 郭 <wanjia_denghuo@sina.com>
2019-12-27 11:26:24 +0800

    Rabbitmq rpc 测试

---
+++ Edu.Models/Models/DealCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EduTest.Controllers.API
+{
+    public class DealCommand
+    {
+        public DealCommand() 
+        {
+            UserIDs = new List<string>();
+        }
+        public Boolean All { get; set; }
+        public String RoomID { get; set; }
+        public DateTime CreateDate { get; set; }
+        public List<String> UserIDs { get; set; }
+    }
+}
---
+++ Edu.Models/Models/JanusRecordMedia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edu.Models.Models
+{
+    public class JanusRecordMedia
+    {
+        public String FileName { get; set; }
+        public String RoomID { get; set; }
+        public String UserID { get; set; }
+        public String CreateTricks { get; set; }
+    }
+}
