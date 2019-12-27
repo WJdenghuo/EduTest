4785bd24d39f93cd96ad5f6a835d921ff6957963
帅 郭 <wanjia_denghuo@sina.com>
2019-12-27 11:31:49 +0800

    rabbitmq 测试

---
+++ Receive/Program.cs
@@ -0,0 +1,156 @@
+﻿using Edu.Models.Models;
+using Edu.Tools;
+using EduTest.Controllers.API;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                //HostName= "106.13.116.83",
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                TopologyRecoveryEnabled = true
+            };
+            using var connection = factory.CreateConnection(new string[]
+            {
+                "106.13.116.83",
+                "62.234.105.58"
+            });
+            using var channel = connection.CreateModel();
+
+            //#region 简单应用
+            //channel.QueueDeclare(queue: "hello1",
+            //                    durable: false,
+            //                    exclusive: false,
+            //                    autoDelete: false,
+            //                    arguments: null);
+            //var consumer = new EventingBasicConsumer(channel);
+            //consumer.Received += (model, ea) =>
+            //{
+            //    var body = ea.Body;
+            //    var message = Encoding.UTF8.GetString(body);
+            //    Console.WriteLine(" [x] Received {0}", message);
+            //};
+            //channel.BasicConsume(queue: "hello1",
+            //                     autoAck: true,
+            //                     consumer: consumer);
+            //#endregion
+
+
+            #region RPC
+            //申明队列接收远程调用请求
+            channel.QueueDeclare(queue: "rpc_queue", durable: false,
+                exclusive: false, autoDelete: false, arguments: null);
+            var consumer = new EventingBasicConsumer(channel);
+            Console.WriteLine("[*] Waiting for message.");
+            //请求处理逻辑
+            consumer.Received += (model, ea) =>
+            {
+                var message = Encoding.UTF8.GetString(ea.Body);
+
+                DealCommand dealCommand = new DealCommand();
+                dealCommand = JsonHelper.Deserialize<DealCommand>(message);
+                if (dealCommand.All)
+                {
+                    Deal();
+                }
+                var test = Directory.GetFiles("/test").Where(x => x.Contains("hasDeal.webm")).ToList();
+                List<JanusRecordMedia> janusRecordMedias = new List<JanusRecordMedia>();
+                test.ForEach(x =>
+                {
+                    janusRecordMedias.Add(new JanusRecordMedia() { FileName = x });
+                });
+                //Thread.Sleep(12000);//模拟耗时
+                //从请求的参数中获取请求的唯一标识，在消息回传时同样绑定
+                var properties = ea.BasicProperties;
+                var replyProerties = channel.CreateBasicProperties();
+                replyProerties.CorrelationId = properties.CorrelationId;
+                //将远程调用结果发送到客户端监听的队列上
+                channel.BasicPublish(exchange: "", routingKey: properties.ReplyTo,
+                basicProperties: replyProerties, body: Encoding.UTF8.GetBytes(JsonHelper.Serialize(test)));
+                //手动发回消息确认
+                channel.BasicAck(ea.DeliveryTag, false);
+                //Console.WriteLine($"Return result: Fib({n})= {result}");
+            };
+            channel.BasicConsume(queue: "rpc_queue", autoAck: false, consumer: consumer);
+            #endregion
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+
+        static void Deal()
+        {
+            var test = Directory.GetFiles("/test").Where(x => Path.GetExtension(x) == ".mjr").ToList();
+            if (test.Count > 0)
+            {
+                test.ForEach(x =>
+                {
+                    Console.WriteLine($"{x}");
+                    if (x.Contains("video.mjr"))
+                    {
+                        Deal("janus-pp-rec", $"{x} {x.Substring(0, x.Length - 3)}webm");
+                    }
+                    else if (x.Contains("audio.mjr"))
+                    {
+                        Deal("janus-pp-rec", $"{x} {x.Substring(0, x.Length - 3)}opus");
+                    }
+                });
+            }
+            test.Where(x => x.Contains("video.mjr")).ToList().ForEach(x =>
+            {
+                Deal("ffmpeg", $"-i {x.Substring(0, x.Length - 9)}audio.opus -i {x.Substring(0, x.Length - 3)}webm  -c:v copy -c:a opus -strict experimental {x.Substring(0, x.Length - 4)}-hasDeal.webm");
+            });
+            Console.WriteLine("Hello World!");
+
+
+        }
+        static void Deal(String fileName, string args)
+        {
+            #region process 测试
+            var psi = new ProcessStartInfo(fileName, args) { RedirectStandardOutput = true };
+            Console.WriteLine($"{fileName} {args}");
+            //启动
+            var proc = Process.Start(psi);
+            if (proc == null)
+            {
+                Console.WriteLine("Can not exec.");
+            }
+            else
+            {
+                Console.WriteLine("-------------Start read standard output--------------");
+                //开始读取
+                using (var sr = proc.StandardOutput)
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        Console.WriteLine(sr.ReadLine());
+                    }
+
+                    if (!proc.HasExited)
+                    {
+                        proc.Kill();
+                    }
+                }
+                Console.WriteLine("---------------Read end------------------");
+                //Console.WriteLine($"Total execute time :{(proc.ExitTime - proc.StartTime).TotalMilliseconds} ms");
+                //Console.WriteLine($"Exited Code ： {proc.ExitCode}");
+            }
+            #endregion
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client.Events;
+using Edu.Tools;
+using EduTest.Controllers.API;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                //HostName = "106.13.116.83",//62.234.105.58,106.13.116.83
+                UserName = "admin",
+                Password = "admin",
+                AutomaticRecoveryEnabled = true,
+                TopologyRecoveryEnabled = true
+            };
+            //集群在connection中添加地址
+            using var connection = factory.CreateConnection(new string[]
+            {
+                "106.13.116.83",
+                "62.234.105.58"
+            });
+            using (var channel = connection.CreateModel())
+            {
+                //#region 简单逻辑
+                ////申明队列(指定durable: true, 告知rabbitmq对消息进行持久化)
+                //channel.QueueDeclare(queue: "hello1",
+                //                     durable: false,
+                //                     exclusive: false,
+                //                     autoDelete: false,
+                //                     arguments: null);
+                ////将消息标记为持久性 - 将IBasicProperties.SetPersistent设置为true
+                //var properties = channel.CreateBasicProperties();
+                //properties.Persistent = true;
+
+                ////设置prefetchCount : 1来告知RabbitMQ，在未收到消费端的消息确认时，不再分发消息，也就确保了当消费端处于忙碌状态时
+                //channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+                //var testdate = DateTime.Now.AddMinutes(1);
+                //for (var i = 1; i < 100000; i++)
+                //{
+                //    string message = $"{i}Hello World!";
+                //    var body = Encoding.UTF8.GetBytes(message);
+
+                //    channel.BasicPublish(exchange: "",
+                //                         routingKey: "hello1",
+                //                         basicProperties: null,
+                //                         body: body);
+
+                //    Console.WriteLine(" [x] Sent {0}", message);
+                //}
+                //#endregion
+
+
+
+                #region RPC
+                // 申明唯一guid用来标识此次发送的远程调用请求
+                var correlationId = Guid.NewGuid().ToString();
+                //申明需要监听的回调队列
+                var replyQueue = channel.QueueDeclare().QueueName;
+                var properties = channel.CreateBasicProperties();
+                properties.ReplyTo = replyQueue;//指定回调队列
+                properties.CorrelationId = correlationId;//指定消息唯一标识
+                //string number = args.Length > 0 ? args[0] : "30";
+                //处理所有文件
+                DealCommand dealCommand = new DealCommand()
+                {
+                    All = true
+                };
+                var body = Encoding.UTF8.GetBytes(JsonHelper.Serialize(dealCommand));
+
+                // //创建消费者用于处理消息回调（远程调用返回结果）
+                var callbackConsumer = new EventingBasicConsumer(channel);
+                channel.BasicConsume(queue: replyQueue, autoAck: true, consumer: callbackConsumer);
+                callbackConsumer.Received += (model, ea) =>
+                {
+                    //仅当消息回调的ID与发送的ID一致时，说明远程调用结果正确返回。
+                    if (ea.BasicProperties.CorrelationId == correlationId)
+                    {
+                        var responseMsg = $"Get Response: {Encoding.UTF8.GetString(ea.Body)}";
+                        Console.WriteLine($"[x]: {responseMsg}");
+                    }
+                };
+
+                /*
+                 * ----------------------------------------
+                 * 
+                 * 发现一个bug 客户端发布消息位于用于回调的消费者之前会出现接收不到
+                 * 远端消费的回调，所以消费要限于生产
+                 * 
+                 * ----------------------------------------
+                 */
+                //发布消息
+                channel.BasicPublish(exchange: "", routingKey: "rpc_queue", basicProperties: properties, body: body);
+                //Console.WriteLine($"[*] Request fib({number})");
+                #endregion
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
